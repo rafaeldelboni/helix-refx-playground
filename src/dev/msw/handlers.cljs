87b62bd7601b017d78bfe05b7b19cf1535e712bd
Rafael Delboni <rafadelboni@gmail.com>
2022-11-02 14:47:28 -0300

    feat(http): better http request

--- src/dev/msw/handlers.cljs
+++ src/dev/msw/handlers.cljs
@@ -2,12 +2,18 @@
   (:require [dev.msw.helpers :refer [resp-post]]))
 
 (defn login-handler [req res ctx]
-  (apply
-   res
-   [(.delay ctx 500)
-    (.status ctx 201)
-    (.json ctx (clj->js {:ok true
-                         :username (-> req .-body .-username)}))]))
+  (if (= (-> req .-body .-username) "err@ee.cc")
+    (apply
+     res
+     [(.delay ctx 1000)
+      (.status ctx 500)
+      (.json ctx (clj->js {:ok false}))])
+    (apply
+     res
+     [(.delay ctx 500)
+      (.status ctx 201)
+      (.json ctx (clj->js {:ok true
+                           :username (-> req .-body .-username)}))])))
 
 (def handlers
   [(resp-post "/login" login-handler)])
