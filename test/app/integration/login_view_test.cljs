b6a312bc77735f92fc0c5f3f8b03e89aef053944
Rafael Delboni <rafadelboni@gmail.com>
2022-11-04 10:43:44 -0300

    chore(lint): better lint rule

--- test/app/integration/login_view_test.cljs
+++ test/app/integration/login_view_test.cljs
@@ -9,15 +9,15 @@
 
 (deftest login-view-test
   (async done
-         (p/do
-           (aux/wait-for
-            #(p/let [_ (msw/start!)
-                     cc (-> auth.views/login-view aux/render)
-                     container (-> cc .-container)
-                     button (-> container (aux/tag "button") first)
-                     input (-> container (aux/query "#login-username"))
-                     _change (-> input (aux/change "err@ee.cc"))
-                     _click (-> button aux/click)
-                     error-msg (-> cc (aux/find-by-text "Error... Try again"))]
-               (is (= "Error... Try again" (aux/text error-msg)))
-               (aux/cleanup done))))))
+    (p/do
+      (aux/wait-for
+       #(p/let [_ (msw/start!)
+                render (-> auth.views/login-view aux/render)
+                container (.-container render)
+                button (-> container (aux/tag "button") first)
+                input (-> container (aux/query "#login-username"))
+                _change (-> input (aux/change "err@ee.cc"))
+                _click (-> button aux/click)
+                error-msg (-> render (aux/find-by-text "Error... Try again"))]
+          (is (= "Error... Try again" (aux/text error-msg)))
+          (aux/cleanup done))))))
